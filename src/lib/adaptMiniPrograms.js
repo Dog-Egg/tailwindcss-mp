7b9cfff870c84f5e3d626c5821a8e6d6168933d0
Lee <294622946@qq.com>
2025-10-18 09:54:38 +0800

    Simplify the way invalid characters are written

--- src/lib/adaptMiniPrograms.js
+++ src/lib/adaptMiniPrograms.js
@@ -1,3 +1,5 @@
+export const invalidChars = ':[]./%'
+
 export default function adaptMiniPrograms(context) {
   const config = context.tailwindConfig
   return (root) => {
@@ -5,8 +7,14 @@ export default function adaptMiniPrograms(context) {
 
     // 转义小程序不支持的选择器字符
     // 例如：.first\:mt-[20px]:first-child -> .first_mt-_20px_:first-child
+    const re = new RegExp(
+      `\\\\[${Array.from(invalidChars)
+        .map((char) => `\\${char}`)
+        .join('')}]`,
+      'g'
+    )
     root.walkRules((rule) => {
-      rule.selector = rule.selector.replace(/\\[:\[\]\%\.\/]/g, '_')
+      rule.selector = rule.selector.replace(re, '_')
     })
   }
 }
--- src/vite/index.js
+++ src/vite/index.js
@@ -1,3 +1,5 @@
+import { invalidChars } from '../lib/adaptMiniPrograms'
+
 export default function modifyClasses() {
   return {
     name: 'modify-classes',
@@ -8,21 +10,21 @@ export default function modifyClasses() {
         return
       }
 
+      const re = new RegExp(
+        `[${Array.from(invalidChars)
+          .map((char) => `\\${char}`)
+          .join('')}]`,
+        'g'
+      )
       // 替换所有 class="..."
       const transformed = code.replace(/class\s*=\s*"([^"]+)"/g, (_, classValue) => {
         const newClasses = classValue
           .split(/\s+/)
-          .map((cls) => replaceInvalidChars(cls))
+          .map((cls) => cls.replace(re, '_'))
           .join(' ')
         return `class="${newClasses}"`
       })
       return transformed
     },
   }
-}
-
-// 自定义替换规则函数
-function replaceInvalidChars(cls) {
-  // 这里按需调整：微信小程序不支持 :, [, ]
-  return cls.replace(/[:\[\]\%\.\/]/g, '_')
 }
